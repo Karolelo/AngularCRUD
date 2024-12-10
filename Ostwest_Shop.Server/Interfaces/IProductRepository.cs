680422cb688eb14d5d8dbb5cae042e6eb38103b9
MroziuOfficial <kubamaciekm22@gmail.com>
2024-12-10 13:10:28 +0100

    Add repository interfaces and implementation for products
    Introduce IRepository and IProductRepository interfaces to define repository contract. Implement ProductRepository class using Entity Framework Core, providing methods for product CRUD operations with category association support.

---
+++ Ostwest_Shop.Server/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ostwest_Shop.Server.Models;
+
+namespace Ostwest_Shop.Server.Interfaces;
+
+public interface IProductRepository
+{
+    Product GetById(int id);
+    
+    IEnumerable<Product> GetAll();
+    void CreateNewProduct(Product product, List<Category> categories);
+    public void UpdateProduct(Product product, List<Category> categories);
+    public void DeleteProduct(Product product);
+}
---
+++ Ostwest_Shop.Server/Repository/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ostwest_Shop.Server.Interfaces;
+using Ostwest_Shop.Server.Models;
+
+namespace Ostwest_Shop.Server.Repository;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly Microsoft.EntityFrameworkCore.DbContext _context;
+
+    public ProductRepository(Microsoft.EntityFrameworkCore.DbContext context)
+    {
+        _context = context;
+    }
+
+    public Product GetById(int id)
+    {
+        return _context.Set<Product>().Find(id) ?? throw new KeyNotFoundException("Product not found.");
+    }
+
+    public IEnumerable<Product> GetAll()
+    {
+        return _context.Set<Product>().ToList();
+    }
+
+    public void CreateNewProduct(Product product, List<Category> categories)
+    {
+        product.Categories = categories;
+        _context.Add(product);
+    }
+    
+    public void UpdateProduct(Product product, List<Category> categories)
+    {
+        product.Categories = categories;
+        _context.Update(product);
+    }
+
+    public void DeleteProduct(Product product)
+    {
+        _context.Remove(product);
+    }
+    
+    
+}
